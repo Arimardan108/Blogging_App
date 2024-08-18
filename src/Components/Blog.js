5f7872533d966c516ce95babf336fabced2c40d0
Arimardan108 <mishraarimardan934@gmail.com>
2024-08-18 16:39:58 +0530

    Update file

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,57 +1,65 @@
 //Blogging App using Hooks
-export default function Blog(){
-    
-    //Passing the synthetic event as argument to stop refreshing the page on submit
-    function handleSubmit(e){
-        e.preventDefault();
-    }
+export default function Blog() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
-    return(
-        <>
-        {/* Heading of the page */}
-        <h1>Write a Blog!</h1>
+  //Passing the synthetic event as argument to stop refreshing the page on submit
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
 
-        {/* Division created to provide styling of section to the form */}
-        <div className="section">
+  return (
+    <>
+      {/* Heading of the page */}
+      <h1>Write a Blog!</h1>
 
+      {/* Division created to provide styling of section to the form */}
+      <div className="section">
         {/* Form for to write the blog */}
-            <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
+          {/* Row component to create a row for first input field */}
+          <Row label="Title">
+            <input
+              className="input"
+              placeholder="Enter the Title of the Blog here.."
+              value={title}
+            />
+          </Row>
 
-                {/* Row component to create a row for first input field */}
-                <Row label="Title">
-                        <input className="input"
-                                placeholder="Enter the Title of the Blog here.."/>
-                </Row >
+          {/* Row component to create a row for Text area field */}
+          <Row label="Content">
+            <textarea
+              className="input content"
+              placeholder="Content of the Blog goes here.."
+              value={content}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </Row>
 
-                {/* Row component to create a row for Text area field */}
-                <Row label="Content">
-                        <textarea className="input content"
-                                placeholder="Content of the Blog goes here.."/>
-                </Row >
+          {/* Button to submit the blog */}
+          <button className="btn">ADD</button>
+        </form>
+      </div>
 
-                {/* Button to submit the blog */}            
-                <button className = "btn">ADD</button>
-            </form>
-                     
-        </div>
+      <hr />
 
-        <hr/>
-
-        {/* Section where submitted blogs will be displayed */}
-        <h2> Blogs </h2>
-        
-        </>
-        )
-    }
+      {/* Section where submitted blogs will be displayed */}
+      <h2> Blogs </h2>
+    </>
+  );
+}
 
 //Row component to introduce a new row section in the form
-function Row(props){
-    const{label} = props;
-    return(
-        <>
-        <label>{label}<br/></label>
-        {props.children}
-        <hr />
-        </>
-    )
+function Row(props) {
+  const { label } = props;
+  return (
+    <>
+      <label>
+        {label}
+        <br />
+      </label>
+      {props.children}
+      <hr />
+    </>
+  );
 }
